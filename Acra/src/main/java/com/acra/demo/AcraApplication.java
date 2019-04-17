69af8c2bc477627287203e2ff3ab34f7cdf66089
Udayabhanu Koleti <UK00616290@techmahindra.com>
2019-04-17 20:14:56 +0530

    Initial Commit

---
+++ Acra/src/main/java/com/acra/demo/AcraApplication.java
@@ -0,0 +1,13 @@
+package com.acra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcraApplication.class, args);
+	}
+
+}
---
+++ Acra/src/test/java/com/acra/demo/AcraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acra.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
