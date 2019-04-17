0d010b0fec43781cdeafe0f0d7ecf78862f97498
UK00616290 <UK00616290@inbasdpc05931.TechMahindra.com>
2019-04-17 22:16:34 +0530

    Modified for War file creation

--- src/main/java/com/acra/demo/AcraApplication.java
+++ src/main/java/com/acra/demo/AcraApplication.java
@@ -2,11 +2,17 @@ package com.acra.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AcraApplication extends SpringBootServletInitializer{
-
+public class AcraApplication extends SpringBootServletInitializer
+{
+	
+	  @Override protected SpringApplicationBuilder
+	  configure(SpringApplicationBuilder application) { return
+	  application.sources(AcraApplication.class);}
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(AcraApplication.class, args);//NOSONAR
 	}
